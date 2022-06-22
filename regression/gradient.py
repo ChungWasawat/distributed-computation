c06019d75524c64838760780e7048823002c6e55
wasawat <bun.wasawat@outlook.com>
2022-06-22 22:57:42 +0100

    add visualisation

--- regression/gradient.py
+++ regression/gradient.py
@@ -8,15 +8,37 @@ from numpy.random import shuffle
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 
+import matplotlib.pyplot as plt
+
 
 def gradient(pos, theta, x, y):
-    
     if pos==0:
         # print(pos, theta[0,pos], y)
         return theta[0,pos] - y
     else:
         # print(pos, theta[0,pos], x[pos-1])
         return gradient(pos-1, theta, x, y) + (theta[0,pos] * x[pos-1])
+  
+def contour(m, path, lr):
+    x,y = m[0], m[1]
+    plt.xlim(-x*3, x*3)
+    plt.ylim(-y*7, y*7)
+    plt.grid()
+    plt.plot(x, y, marker="o", markersize=10, markeredgecolor="blue", markerfacecolor="white")
+    
+    for i in range(path.shape[0]-1):
+        if i == path.shape[0]-2:
+            plt.annotate('', xy=path[i + 1, :], xytext=path[i, :],
+                         arrowprops={'arrowstyle': '->', 'color': 'green', 'lw': 1},
+                         va='center', ha='center')
+        else:
+            plt.annotate('', xy=path[i + 1, :], xytext=path[i, :],
+                         arrowprops={'arrowstyle': '->', 'color': 'red', 'lw': 1},
+                         va='center', ha='center')            
+        
+    title = f"learning rate = {lr}"
+    plt.title(title)
+    plt.show()
     
 
 """
@@ -48,22 +70,27 @@ X = std_scaler.fit_transform(X)
 
 model = LinearRegression()
 model.fit(X, y)
-print(model.intercept_, model.coef_)
+
+print("from linear regression", model.intercept_, model.coef_)
 
 ###### gradient descent
 
-learning_rate = [0.001, 0.005, 0.01, 0.05, 0.1]
+learning_rate = [0.005, 0.01, 0.05, 0.1]
 epoch = 2
 batch_size = 128
-nrows = 1000#X.shape[0]
+nrows = 1280#X.shape[0]
 
 all_data = np.c_[X,y]
 train_x = all_data[:, :-1]
 train_y = all_data[:, -1]
 
+old_theta = []
+
 for lr in learning_rate:
     seed(47)
     theta = randn(1, X.shape[1]+1)
+    old_th = []
+    old_th.append(theta[:,1:].reshape(2,))
     for i in range(epoch):
         # shuffle(all_data)
         # train_x = all_data[:, :-1]
@@ -77,12 +104,19 @@ for lr in learning_rate:
                     temps = theta[0,j] - (lr * grad)
                 else:
                     temp = theta[0,j] - (lr * grad * train_x[start,j-1] )
-                    temps = np.c_[temps,temp]
-                    
+                    temps = np.c_[temps,temp]        
             theta = temps
-    print(lr, theta)
-        
-        
+            if (start+1) % (batch_size) == 0:
+                old_th.append(theta[:,1:].reshape(2,))
+    print("learning rate=", lr, theta.flatten())
+    old_theta.append(old_th)    
+
+
+# visualisation
+for lr in range(len(learning_rate)):
+    all_w = np.array(old_theta[lr])
+    contour(model.coef_, all_w, learning_rate[lr])    
+
 ####### stochastic gradient descent
 
 
