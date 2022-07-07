11c80367785756af7d635ee0b52a0f3e8c0fd297
wasawat <bun.wasawat@outlook.com>
2022-07-07 18:26:29 +0100

    add check if graph is entirely conneted

--- regression/grad_decen.py
+++ regression/grad_decen.py
@@ -6,11 +6,12 @@ import numpy as np
 import pandas as pd
 from numpy.random import seed
 from numpy.random import randn
+from numpy.random import uniform
 from numpy.random import shuffle
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
+#from sklearn.linear_model import LinearRegression
+#from sklearn.preprocessing import StandardScaler
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def theta_init(seed_num: int, row: int) -> np.array:
     seed(seed_num)
@@ -61,4 +62,75 @@ def converge(error, step, lr):
     plt.ylabel("error")
     plt.show()
 
-def random_network()
+def random_network(node: int, p: float) -> np.array:
+    nw = np.zeros((node,node))
+
+    route = {}
+    for i in range(node):
+        route[i] = []
+
+    c = (node **2)
+    for i in range(node-1,0,-1):
+        c -= i
+
+    i,j = 0,0
+    while c > 0:
+        if i==j:
+            nw[i,j] = 1
+            nw[j,i] = 1
+        else:
+            x = uniform(0,1)
+            if x <= p:
+                nw[i,j] = 1
+                nw[j,i] = 1 
+                if j not in route[i]:
+                    route[i].append(j)  
+                    route[j].append(i)
+        j+=1
+        if j == node:
+            i += 1
+            j = i
+        c -=1
+    return nw, route
+
+def visit_adj(adj_m, node, neighbour, visited):
+    if adj_m[node] == []:
+        print("case0", node, neighbour, "nothing", visited)
+        return visit_adj(adj_m, node+1, neighbour, visited) 
+    elif adj_m[node] == [] and node == len(adj_m.keys())-1:
+        print("case0.5", node, neighbour, "nothing", visited)
+        return visited 
+    if visited[node] == 0:
+        visited[node] = 1
+    if visited[adj_m[node][neighbour]] ==1 and adj_m[node][neighbour] == adj_m[node][-1]:
+        print("case1", node, neighbour, adj_m[node][neighbour], visited)
+        return visited
+    elif visited[adj_m[node][neighbour]] ==1 :
+        print("case2", node, neighbour, adj_m[node][neighbour], visited)
+        return visit_adj(adj_m, node, neighbour+1, visited) ##
+    elif visited[adj_m[node][neighbour]] ==0 and adj_m[node][neighbour] == adj_m[node][-1]:
+        print("case3", node, neighbour, adj_m[node][neighbour], visited)
+        return visit_adj(adj_m, adj_m[node][neighbour], 0, visited)
+    else:
+        print("case4", node, neighbour, adj_m[node][neighbour], visited)
+        visited = visit_adj(adj_m, adj_m[node][neighbour], 0, visited) ##
+        return visit_adj(adj_m, node, neighbour+1, visited)
+
+
+
+
+
+
+
+a,b = random_network(5, 0.5)
+b = {0: [1], 1: [0, 2, 4], 2: [1, 4], 3: [], 4: [1, 2]}
+#print(a, b)
+print(b)
+
+#for c in b.keys():
+#    print(c, group_adj(b, c, 0))
+
+for c in b.keys():
+    print(c, visit_adj(0, b[c], []))
+
+print("new", visit_adj2(b, 0, 0, [0]*5))
